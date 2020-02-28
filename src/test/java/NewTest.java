a9e01a03bc6365e1513ba51c19f96d32bb6554a7
klara shvartzman <mymobilek@gmail.com>
2020-02-28 09:14:52 +0200

    k

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() throws InterruptedException {
+	  System.setProperty("webdriver.chrome.driver", "/Users/k/Downloads/chromedriver");
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://google.com");
+	  Thread.sleep(3000);
+	  driver.quit();
+  }
+  @Test()
+  public void test1() {
+	  System.out.println("test1");
+	  
+	  long id = Thread.currentThread().getId();
+	  System.out.println("Test 1" + id);
+  }
+	}
